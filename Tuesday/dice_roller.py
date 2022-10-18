b15355a176d14d445f9bbe6414aff8bf411e1e6f
kostyantyn <funyourmind@gmail.com>
2022-10-18 14:27:56 +0200

    add dice_roller_func.py

--- Tuesday/dice_roller.py
+++ Tuesday/dice_roller.py
@@ -3,17 +3,4 @@ from random import randint
 dice_number = int(input("How many dice are we rolling? "))
 dice_sides = int(input("How many sides on each die? "))
 
-def roll():
-    for x in range(dice_number):
-        number = randint(1,dice_sides)
-        print(f"|{number}|", end="")
 
-roll()
-print()
-user_input = input("Roll again? ('q' to quit) ")
-while user_input != "q":
-    roll()
-    print()
-    user_input = input("Roll again? ('q' to quit) ")
-else:
-    exit()
