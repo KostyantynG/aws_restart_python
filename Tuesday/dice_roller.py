6faca16865b517a4bc967987f828520175a0b84c
kostyantyn <funyourmind@gmail.com>
2022-10-18 14:43:13 +0200

    final version dice_roller.py

--- Tuesday/dice_roller.py
+++ Tuesday/dice_roller.py
@@ -1,6 +1,24 @@
 from random import randint
 
 dice_number = int(input("How many dice are we rolling? "))
+while dice_number > 20:
+    print("Please enter an integer from 1 to 20: ")
+    dice_number = int(input("How many dice are we rolling? "))
+
 dice_sides = int(input("How many sides on each die? "))
+while dice_sides > 20:
+    print("Please enter an integer from 1 to 20: ")
+    dice_sides = int(input("How many dice are we rolling? "))
 
+for die in range(dice_number):
+    number = randint(1,dice_sides)
+    print(f"|{number}|", end="")
+print()
 
+user_input = input("Roll again? ('q' to quit) ")
+while user_input != "q":
+    for die in range(dice_number):
+        number = randint(1,dice_sides)
+        print(f"|{number}|", end="")
+    print()
+    user_input = input("Roll again? ('q' to quit) ")
--- Tuesday/dice_roller_func.py
+++ Tuesday/dice_roller_func.py
@@ -1,7 +1,14 @@
 from random import randint
 
 dice_number = int(input("How many dice are we rolling? "))
+while dice_number > 20:
+    print("Please enter an integer from 1 to 20: ")
+    dice_number = int(input("How many dice are we rolling? "))
+    
 dice_sides = int(input("How many sides on each die? "))
+while dice_sides > 20:
+    print("Please enter an integer from 1 to 20: ")
+    dice_sides = int(input("How many dice are we rolling? "))
 
 def roll():
     for x in range(dice_number):
