04093b8b298b5e734d8f3b4718a20c9322091311
kostyantyn <funyourmind@gmail.com>
2022-10-18 14:48:37 +0200

    add comments to dice_roller.py

--- Tuesday/dice_roller.py
+++ Tuesday/dice_roller.py
@@ -1,21 +1,26 @@
+# Import randint
 from random import randint
 
+# Ask the number of dice
 dice_number = int(input("How many dice are we rolling? "))
 while dice_number > 20:
     print("Please enter an integer from 1 to 20: ")
     dice_number = int(input("How many dice are we rolling? "))
 
+# Ask the number of sides for each die
 dice_sides = int(input("How many sides on each die? "))
 while dice_sides > 20:
     print("Please enter an integer from 1 to 20: ")
     dice_sides = int(input("How many dice are we rolling? "))
 
+# Roll all dice single time and ask for another round
 for die in range(dice_number):
     number = randint(1,dice_sides)
     print(f"|{number}|", end="")
 print()
-
 user_input = input("Roll again? ('q' to quit) ")
+
+# Check if user wants to continue and roll the dice infinitely until user inputs 'q'
 while user_input != "q":
     for die in range(dice_number):
         number = randint(1,dice_sides)
