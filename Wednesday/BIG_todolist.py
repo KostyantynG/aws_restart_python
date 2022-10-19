fd8fc2e82b7c5825753970ee2dc6f3a72f7fcc2a
kostyantyn <funyourmind@gmail.com>
2022-10-19 12:46:21 +0200

    small changes to BIG_todolist.py

--- Wednesday/BIG_todolist.py
+++ Wednesday/BIG_todolist.py
@@ -5,11 +5,11 @@ def user_prompt():
     user_input = input("> ")
     return user_input
 
-def printing_tasks(todos):
+def print_tasks(todos):
     for index, item in enumerate(todos, 1):
         print(f"{index}) {item}")
 
-def printing_help():
+def print_help():
     print("""
 TODO LIST HELP
 Type 'q' or 'quit' to quit
@@ -31,20 +31,20 @@ print()
 user_prompt()
 
 while user_input != "q" and user_input != "quit":
-    
+
     if user_input == "h":
-        printing_help()
+        print_help()
         user_prompt()
     elif user_input == "":
-        printing_tasks(todos)
+        print_tasks(todos)
         user_prompt()
     elif user_input.isdigit() and 0 < int(user_input) <= len(todos):
         todos.pop(int(user_input)-1)
-        printing_tasks(todos)
+        print_tasks(todos)
         user_prompt()
     else:
         todos.append(user_input)
-        printing_tasks(todos)
+        print_tasks(todos)
         user_prompt()
 
-printing_tasks(todos)
+print_tasks(todos)
