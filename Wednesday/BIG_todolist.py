e2b0aac139677ebff100ac5770f3fc323dd97b2d
kostyantyn <funyourmind@gmail.com>
2022-10-19 11:59:40 +0200

    finished rough version of BIG_todolist.py

--- Wednesday/BIG_todolist.py
+++ Wednesday/BIG_todolist.py
@@ -1,3 +1,5 @@
+from curses.ascii import isdigit
+
 print("""
   _____         _           
  |_   _|__   __| | ___  ___ 
@@ -12,12 +14,24 @@ print("***********************************")
 print("Enter a command. Type 'h' for help: ")
 user_input = input("> ")
 todos = []
-while user_input != "q":
+while user_input != "q" and user_input != "quit":
     if user_input == "h":
-        print("""TODO LIST HELP
-        Type 'q' to quit
-        To add a todo to the list, type it and hit enter
-        To complete a todo enter its number""")
+        print("""
+TODO LIST HELP
+Type 'q' to quit
+To add a todo to the list, type it and hit enter
+To complete a todo enter its number""")
+        print("***********************************")
+        print("Enter a command. Type 'h' for help: ")
+        user_input = input("> ")
+    elif user_input == "":
+        print("***********************************")
+        print("Enter a command. Type 'h' for help: ")
+        user_input = input("> ")
+    elif user_input.isdigit() and 0 < int(user_input) <= len(todos):
+        todos.pop(int(user_input)-1)
+        for index, item in enumerate(todos, 1):
+            print(f"{index}) {item}")
         print("***********************************")
         print("Enter a command. Type 'h' for help: ")
         user_input = input("> ")
@@ -28,4 +42,5 @@ while user_input != "q":
         print("***********************************")
         print("Enter a command. Type 'h' for help: ")
         user_input = input("> ")
-        
+for index, item in enumerate(todos, 1):
+    print(f"{index}) {item}")
